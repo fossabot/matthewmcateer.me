4e716f3e7607b388016f386c4b91753df1b35761
Matthew McAteer <matthewmcateer0@gmail.com>
2019-05-26 00:04:10 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,43 @@
+'use strict';
+
+module.exports = {
+  url: 'https://matthewmcateer.me',
+  title: 'matthewmcateer.me',
+  subtitle: 'Machine Learning Engineer at UnifyID. Former aging researcher. I write about about probabilistic programming, machine learning, biotech, distributed systems, dogs, and more.',
+  copyright: '© Matthew McAteer 2019',
+  postsPerPage: 5,
+  googleAnalyticsId: 'UA-102946688-1',
+  menu: [
+    {
+      label: 'Blog',
+      path: '/'
+    },
+    {
+      label: 'About',
+      path: '/about/'
+    },
+    {
+      label: 'Portfolio',
+      path: '/portfolio/'
+    },
+    {
+      label: 'Publications',
+      path: '/publications/'
+    },
+    {
+      label: 'Bookshelf',
+      path: '/bookshelf/'
+    },
+  ],
+  author: {
+    name: 'Matthew McAteer',
+    photo: '/photo.png',
+    photoLarge: '/photo-large.png',
+    bio: 'Machine Learning Engineer at UnifyID. Former aging researcher. I write about about probabilistic programming, machine learning, biotech, distributed systems, dogs, and more.',
+    contacts: {
+      email: 'matthewmcateer0@gmail.com',
+      twitter: 'MatthewMcateer0',
+      github: 'matthew-mcateer',
+    }
+  }
+};
